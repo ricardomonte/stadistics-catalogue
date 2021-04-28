383972ff4643c141903d3908d753076ebe80d3fd
ricardo <ramn21@gmail.com>
2021-04-28 13:08:31 +0200

    Add api calls

---
+++ src/API/Api.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+export const getGames = async () => {
+  const url = 'http://localhost:5000/games';
+  try {
+    const res = await axios.get(url);
+    return res.data;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const getGamesImages = async () => {
+  const urlGame = 'http://localhost:5000/games/images';
+  try {
+    const res = await axios.get(urlGame);
+    return res.data
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getGamesPlatforms = async () => {
+  const urlGame = 'http://localhost:5000/games/platforms';
+  try {
+    const res = await axios.get(urlGame)
+    return res.data
+  } catch (error){
+    console.log(error)
+  }
+}
