490e0f89d881eff63e3766a8db94a2ec9ece685b
ricardo <ramn21@gmail.com>
2021-04-28 14:24:29 +0200

    Add useeffect and proptypes

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
-const Games = () => (
+const Games = ({ game }) => (
   <div>
     <img alt="something" />
     <div>
-      <h4>title</h4>
+      <h4>{game.title}</h4>
       <p>some text</p>
       <p />
     </div>
   </div>
 );
+
+Games.protoTypes = {
+  game: PropTypes.instanceOf(Object).isRequired,
+};
 
 export default Games;
--- src/container/GameList.js
+++ src/container/GameList.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Games from '../components/Games';
 import { loadGames } from '../actions';
 
-const GameList = ({ games, loadedGames }) => (
-  <div>
-    <h1>List of games</h1>
-    <Games />
-  </div>
-);
+const GameList = ({ games, loadedGames }) => {
+  useEffect(() => {
+    loadedGames().catch((error) => {
+      alert(`loading games failed ${error}`);
+    });
+  }, []);
+
+  return (
+    <div>
+      <h1>List of games</h1>
+      {games.map((game) => (
+        <Games key={game.title} game={game} />
+      ))}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
@@ -20,6 +31,11 @@ const mapDispatchToProps = (dispatch) => {
   return {
     loadedGames: () => dispatch(loadGames()),
   };
+};
+
+GameList.protoTypes = {
+  games: PropTypes.instanceOf(Array).isRequired,
+  loadGames: PropTypes.func.isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(GameList);
