405dbbff1b5065d9813a89f3495c48dcfb3cf7f5
ricardo <ramn21@gmail.com>
2021-04-28 14:12:25 +0200

    Add conection with redux

--- src/container/GameList.js
+++ src/container/GameList.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import Games from '../components/Games';
+import { loadGames } from '../actions';
 
-const GameList = () => (
+const GameList = ({ games, loadedGames }) => (
   <div>
     <h1>List of games</h1>
     <Games />
   </div>
 );
 
-export default GameList;
+const mapStateToProps = (state) => {
+  return {
+    games: state.game,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    loadedGames: () => dispatch(loadGames()),
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(GameList);
