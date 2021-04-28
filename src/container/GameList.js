3f90a867a311f6dfcb820d784e56d0197d3e791d
ricardo <ramn21@gmail.com>
2021-04-28 13:41:32 +0200

    Add Game component

--- src/container/GameList.js
+++ src/container/GameList.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import Games from '../components/Games';
 
-const GameList = () => {
-  <h1>List of games</h1>;
-};
+const GameList = () => (
+  <div>
+    <h1>List of games</h1>
+    <Games />
+  </div>
+);
 
 export default GameList;
