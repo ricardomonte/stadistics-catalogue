e36cc53d586fad58e0ed0d0a205a3d4dc62fd62e
ricardo <ramn21@gmail.com>
2021-04-28 17:04:06 +0200

    Change div for Link

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const Games = ({ game }) => (
-  <div>
+  <Link to={`/game/${game.slug}`}>
     <img alt="something" src={`https:${game.cover}`} />
     <div>
       <h4>{game.name}</h4>
       <p>{game.summary}</p>
       <p />
     </div>
-  </div>
+  </Link>
 );
 
 Games.propTypes = {
