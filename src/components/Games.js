37c0bee82d5bdc8074c6e93880ad64863ad224f4
ricardo <ramn21@gmail.com>
2021-04-28 13:41:02 +0200

    Create Game.js

---
+++ src/components/Games.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Games = () => (
+  <div>
+    <img alt="something" />
+    <div>
+      <h4>title</h4>
+      <p>some text</p>
+      <p />
+    </div>
+  </div>
+);
+
+export default Games;
