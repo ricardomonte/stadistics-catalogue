6bb51e5d7d0060a272fad54c0f8f51d38b45e9c8
ricardo <ramn21@gmail.com>
2021-04-28 13:40:37 +0200

    Add route

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,10 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import GameList from '../container/GameList';
 
-const App = () => {
-  return (
-    <div>
-      <Route exact path="/" component={GameList} />
-    </div>
-  );
-};
+const App = () => (
+  <div>
+    <Route exact path="/" component={GameList} />
+  </div>
+);
 
 export default App;
