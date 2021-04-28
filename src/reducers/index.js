6c29964e56629672aa79770931811c824eba82f8
ricardo <ramn21@gmail.com>
2021-04-28 17:16:09 +0200

    Add image to the root reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import game from './game';
+import images from './game';
 
 const rootReducer = combineReducers({
   game,
+  images,
 });
 
 export default rootReducer;
