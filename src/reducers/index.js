f8471afe1550fa0dbfc8bb2c1ac09eb8e5822fbe
ricardo <ramn21@gmail.com>
2021-04-29 17:34:23 +0200

    Add filter to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import game from './game';
 import image from './images';
+import filter from './filter';
 
 const rootReducer = combineReducers({
   game,
   image,
+  filter,
 });
 
 export default rootReducer;
