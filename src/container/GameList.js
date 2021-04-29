69f2821cfcd5a2d49797f5dfdcaca2546712e92b
ricardo <ramn21@gmail.com>
2021-04-29 17:39:14 +0200

    Add filterGame, handleFilterChange logic to filter

--- src/container/GameList.js
+++ src/container/GameList.js
@@ -2,19 +2,32 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import Games from '../components/Games';
-import { loadGames } from '../actions';
+import GenreFilter from '../components/GenreFilter';
+import { loadGames, filterGame } from '../actions';
 
-const GameList = ({ games, loadedGames }) => {
+const GameList = ({
+  games, loadedGames, filteredGames, filteredGame,
+}) => {
   useEffect(() => {
     loadedGames().catch((error) => {
       alert(`loading games failed ${error}`);
     });
   }, []);
+  const handleFilterChange = (event) => {
+    filteredGames(event.target.value);
+  };
+
+  const oneGenreGames = () => games.filter((game) => game.genre === filteredGame.filter);
+  const filterByGenre = filteredGame.filter !== '' ? oneGenreGames() : games;
 
   return (
     <div>
+      <GenreFilter
+        filter={filteredGame.filter}
+        onGameChange={handleFilterChange}
+      />
       <h1>List of games</h1>
-      {games.map((game) => (
+      {filterByGenre.map((game) => (
         <Games key={game.name} game={game} />
       ))}
     </div>
@@ -23,15 +36,19 @@ const GameList = ({ games, loadedGames }) => {
 
 const mapStateToProps = (state) => ({
   games: state.game,
+  filteredGame: state.filter,
 });
 
 const mapDispatchToProps = (dispatch) => ({
   loadedGames: () => dispatch(loadGames()),
+  filteredGames: (filter) => dispatch(filterGame(filter)),
 });
 
 GameList.propTypes = {
   games: PropTypes.instanceOf(Array).isRequired,
   loadedGames: PropTypes.func.isRequired,
+  filteredGames: PropTypes.func.isRequired,
+  filteredGame: PropTypes.instanceOf(Object).isRequired,
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(GameList);
