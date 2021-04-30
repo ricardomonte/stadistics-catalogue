8cc427bfd621b802b76b871ea465a3ecc6686afd
ricardo <ramn21@gmail.com>
2021-04-30 21:08:53 +0200

    Add about and Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import NavBar from '../common/NavBar';
+import About from '../common/About';
 import GameList from '../container/GameList';
 import GameOnly from '../container/GameOnly';
 
 const App = () => (
-  <div>
-    <Switch>
-      <Route exact path="/" component={GameList} />
-      <Route path="/game/:id" component={GameOnly} />
-    </Switch>
-  </div>
+  <>
+    <NavBar />
+    <div>
+      <Switch>
+        <Route exact path="/" component={GameList} />
+        <Route path="/about" component={About} />
+        <Route path="/game/:id" component={GameOnly} />
+      </Switch>
+    </div>
+  </>
 );
 
 export default App;
