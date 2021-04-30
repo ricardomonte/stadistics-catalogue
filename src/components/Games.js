a2475d2c49f9406737a9b4bc290606661fbae898
ricardo <ramn21@gmail.com>
2021-04-30 21:09:58 +0200

    Add Games return element

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import GameStyle from '../styles/Game.module.css';
 
-const Games = ({ game }) => (
-  <Link to={`/game/${game.id}`}>
-    <img alt="something" src={`https:${game.cover}`} />
-    <div>
-      <h4>{game.name}</h4>
-      <p />
+const Games = ({ game }) => {
+  const [hover, setHover] = useState(true);
+  const onHover = () => {
+    setHover(true);
+  };
+  const offHover = () => {
+    setHover(false);
+  };
+
+  return (
+    <div
+      className={GameStyle.game}
+      onMouseEnter={offHover}
+      onMouseLeave={onHover}
+    >
+      <Link to={`/game/${game.id}`} className={GameStyle.gameLink}>
+        <img
+          alt="something"
+          src={`https:${game.cover}`}
+          className={GameStyle.gameImage}
+        />
+        <div className={hover ? GameStyle.gameContainer : GameStyle.invisible}>
+          <h3 className={GameStyle.gameTitle}>{game.name}</h3>
+        </div>
+      </Link>
     </div>
-  </Link>
-);
+  );
+};
 
 Games.propTypes = {
   game: PropTypes.instanceOf(Object).isRequired,
