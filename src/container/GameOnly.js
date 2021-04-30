abf2f70bde8fa1cd7d2fa7644f92e01e58b97b6d
ricardo <ramn21@gmail.com>
2021-04-30 21:11:16 +0200

    Add styles

--- src/container/GameOnly.js
+++ src/container/GameOnly.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { loadScreenshots } from '../actions';
+import GameStyle from '../styles/OneGame.module.css';
 
 const GameOnly = ({ match, loadedOneGame, images }) => {
   useEffect(() => {
@@ -10,58 +12,80 @@ const GameOnly = ({ match, loadedOneGame, images }) => {
       alert(`loading games failed ${error}`);
     });
   }, []);
+  const history = useHistory();
+  const goBack = () => {
+    history.push('/');
+  };
 
   return (
-    <div>
-      <img alt="something" src={`https:${images.cover}`} />
-      <div>
-        <h4>{images.name}</h4>
-        <p>{images.genres}</p>
-        <h3>{images.storyline !== '' ? 'Storyline' : 'Summary'}</h3>
-        <p>{images.storyline !== '' ? images.storyline : images.summary}</p>
-        <div>
-          <h4>Screenshots</h4>
-          <img alt="something" src={`https:${images.screen1}`} />
+    <div className={GameStyle.gameTotal}>
+      <img
+        alt="something"
+        src={`https:${images.cover}`}
+        className={GameStyle.gameImage}
+      />
+      <div className={GameStyle.gameContainer}>
+        <div className={GameStyle.gameInfo}>
+          <h1>{images.name}</h1>
+          <p className={GameStyle.genre}>{images.genre}</p>
+          <h2>{images.storyline !== '' ? 'Storyline' : 'Summary'}</h2>
+          <p className={GameStyle.info}>
+            {images.storyline !== '' ? images.storyline : images.summary}
+          </p>
+        </div>
+        <div className={GameStyle.gameScreen}>
+          <h2 className={GameStyle.gameScreenTitle}>Screenshots</h2>
+          <img
+            alt="something"
+            src={`https:${images.screen1}`}
+            className={GameStyle.gameScreenImg}
+          />
           <img
             alt="something"
             src={`https:${
               images.screen2 !== '' ? images.screen2 : images.screen1
             }`}
+            className={GameStyle.gameScreenImg}
           />
           <img
             alt="something"
             src={`https:${
               images.screen3 !== '' ? images.screen3 : images.screen1
             }`}
+            className={GameStyle.gameScreenImg}
           />
         </div>
-        <div>
-          <h4>Platforms</h4>
-          <div>
-            <img alt="something" src={`https:${images.PClogo}`} />
-            <p>{images.PC}</p>
-            <div>
-              <h5>Release:</h5>
-              <p>{images.PCrelease}</p>
+        <div className={GameStyle.gamePlatforms}>
+          <h2>Platforms</h2>
+          <div className={GameStyle.gameImgPlat}>
+            <div className={GameStyle.gamePlatLogo}>
+              <img alt="something" src={`https:${images.PClogo}`} />
+              <div>
+                <h5>Release:</h5>
+                <p>{images.PCrelease}</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <img alt="something" src={`https:${images.PS4logo}`} />
-            <p>{images.PS4}</p>
-            <div>
-              <h5>Release:</h5>
-              <p>{images.PS4release}</p>
+            <div className={GameStyle.gamePlatLogo}>
+              <img alt="something" src={`https:${images.PS4logo}`} />
+              <div>
+                <h5>Release:</h5>
+                <p>{images.PS4release}</p>
+              </div>
             </div>
-          </div>
-          <div>
-            <img alt="something" src={`https:${images.XONElogo}`} />
-            <p>{images.XONE}</p>
-            <div>
-              <h5>Release:</h5>
-              <p>{images.XONErelease}</p>
+            <div className={GameStyle.gamePlatLogo}>
+              <img alt="something" src={`https:${images.XONElogo}`} />
+              <div>
+                <h5>Release:</h5>
+                <p>{images.XONErelease}</p>
+              </div>
             </div>
           </div>
         </div>
+      </div>
+      <div className={GameStyle.containerBtn}>
+        <button className={GameStyle.backBtn} type="button" onClick={goBack}>
+          Go Back
+        </button>
       </div>
     </div>
   );
