1b7dff230cf63c3ed4334844efcdfd20a65e794f
ricardo <ramn21@gmail.com>
2021-05-01 02:12:44 +0200

    Add real url for api

--- src/API/Api.js
+++ src/API/Api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const getGames = async () => {
-  const url = 'http://localhost:5000/games';
+  const url = 'http://www.mybaseapi.xyz/games';
   try {
     const res = await axios.get(url);
     return res.data;
@@ -11,7 +11,7 @@ export const getGames = async () => {
 };
 
 export const getGamesImages = async (id) => {
-  const urlGame = `http://localhost:5000/games/${id}`;
+  const urlGame = `http://www.mybaseapi.xyz/games/${id}`;
   try {
     const res = await axios.get(urlGame);
     return await res.data[0];
