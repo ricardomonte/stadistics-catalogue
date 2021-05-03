8b6ba05758e6f506868350b7ef45c55de0666af6
ricardo <ramn21@gmail.com>
2021-05-03 22:21:06 +0200

    Add mageErr to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import game from './game';
 import image from './images';
 import filter from './filter';
+import manageErr from './managesErrors';
 
 const rootReducer = combineReducers({
   game,
   image,
   filter,
+  manageErr,
 });
 
 export default rootReducer;
